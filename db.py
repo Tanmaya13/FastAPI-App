23b2f4a5a4ad25535d8dc1ecda67dad51e96412f
Tanmaya Nayak <98599230+Tanmaya13@users.noreply.github.com>
2024-10-11 16:56:26 +0530

    database configurations

--- db.py
+++ db.py
@@ -1,14 +1,17 @@
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 
-username = "tanmaya"
-password = "Tanmaya@mongo"
+'''use for cloud MongoDB connections'''
+#username = "username"
+#password = "password"
+#escaped_username = quote_plus(username)
+#escaped_password = quote_plus(password)
+#mongo_uri = f"mongodb+srv://{escaped_username}:{escaped_password}@cluster0.6yvxr.mongodb.net/"
+#client = MongoClient(mongo_uri)
 
-escaped_username = quote_plus(username)
-escaped_password = quote_plus(password)
-
-mongo_uri = f"mongodb+srv://{escaped_username}:{escaped_password}@cluster0.6yvxr.mongodb.net/"
-client = MongoClient(mongo_uri)
+'''for local MongoDB connection'''
+mongoURI = "mongodb://localhost:27017"
+client = pymongo.MongoClient(mongoURI)
 
 db = client["MyDB"]
 items_collection = db["items"]
